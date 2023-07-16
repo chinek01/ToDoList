3e5d00bfca696c7cd4c56f2909274b7099a596e9
MarcinC <chinek01@gmail.com>
2023-07-16 19:19:24 +0200

    Register form

---
+++ Forms/Forms.py
@@ -0,0 +1,25 @@
+"""
+
+
+Forms for ToDo_list project
+
+#100DaysOfCode with Python
+Day: 87
+Date: 2023-07-16
+Author: MC
+
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForm
+class RegisterForm(FlaskForm):
+    email = StringField("Email: ", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    name = StringField("Name: ", validators=[DataRequired()])
+    submit = SubmitField("Create user")
+
